0e7683e6e579788ab46e71183e7d0ca559ca20e3
andrews88l <andrews.luke@gmail.com>
2016-01-27 12:34:48 -0500

    created the contact detail view

--- crmapp/contacts/views.py
+++ crmapp/contacts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+from .models import Contact
 
 # Create your views here.
+
+@login_required()
+def contact_detail(request, uuid):
+
+	contact = Contact.objects.get(uuid=uuid)
+
+	return render(request, 'contacts/contact_detail.html', {'contact' : contact})
